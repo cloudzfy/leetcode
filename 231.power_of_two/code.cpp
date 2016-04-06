c6f0708296e4afe84990e5eaea3a75e3ad8d735d
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-06 17:48:29 +0800

    Create code.cpp

---
+++ 231.power_of_two/code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        int count = 0;
+        while(n != 0) {
+            if (n % 2) count++;
+            n = n >> 1;
+        }
+        if (count <= 1) return true;
+        return false;
+    }
+};
