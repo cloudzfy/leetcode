253564fb46f9252280cbe59f03719e4a856ffc7e
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-12 21:32:36 +0800

    Create code.cpp

---
+++ 169.majority_element/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate, count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count++;
+            } else if (candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
