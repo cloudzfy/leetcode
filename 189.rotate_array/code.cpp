2b92f56891f6f12b7849dec7837aec2a29439af4
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-15 17:46:32 +0800

    Create code.cpp

--- 189.rotate_array/code.cpp
+++ 189.rotate_array/code.cpp
@@ -3,12 +3,13 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        for (int i = 0; i < k; i++) {
-            int tmp = nums[n - 1];
-            for (int j = n - 1; j > 0; j--) {
-                nums[j] = nums[j - 1];
-            }
-            nums[0] = tmp;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+    void reverse(vector<int>& nums, int left, int right) {
+        while (left < right) {
+            swap(nums[left++], nums[right--]);
         }
     }
 };
