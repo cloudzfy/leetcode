d355329ba25b6ab356d420517bc35899abf53c6f
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-21 16:05:36 +0800

    Create code.cpp

--- 008.string_to_integer_atoi/code.cpp
+++ 008.string_to_integer_atoi/code.cpp
@@ -1,77 +1,24 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        int ans = 0;
-        int max = 2147483647, min = -2147483648;
-        bool sym = false, start = false, illegal = false, pos = true;
-        for(int i = 0; i < str.length(); i++)
-        {
-            if (str[i] == ' ')
-            {
-                if (!start) continue;
-                else break;
-            }
-            else start = true;
-            if (str[i] == '+')
-            {
-                if (!sym)
-                {
-                    sym = true;
-                    pos = true;
-                    continue;
-                }
-                else
-                {
-                    illegal = true;
-                    break;
-                }
-            }
-            if (str[i] == '-')
-            {
-                if (!sym)
-                {
-                    sym = true;
-                    pos = false;
-                    continue;
-                }
-                else
-                {
-                    illegal = true;
-                    break;
-                }
-            }
-            if (str[i] > '9' || str[i] < '0') break;
-            if (ans == min / 10)
-            {
-                if (pos && str[i] - '0' > 7)
-                {
-                    ans = -max;
-                    continue;
-                }
-                else if (!pos && str[i] - '0' > 8)
-                {
-                    ans = min;
-                    continue;
-                }
-            }
-            if (ans < min / 10)
-            {
-                if (pos) ans = -max;
-                if (!pos) ans = min;
-                continue;
-            }
-            ans *= 10;
-            ans -= str[i] - '0';
+        if (str.length() == 0) return 0;
+        int sign = 1;
+        int i = 0;
+        long long num = 0;
+        while (str[i] == ' ') i++;
+        if (str[i] == '-') {
+            sign *= -1;
+            i++;
+        } else if (str[i] == '+') i++;
+        bool checkSign = false;
+        for (;i < str.length(); i++) {
+            if (str[i] <= '9' && str[i] >= '0') {
+                if (sign == 1) num = num * 10 + (str[i] - '0');
+                else num = num * 10 - (str[i] - '0');
+            } else break;
+            if (sign == 1 && (num < 0 || num > INT_MAX)) return INT_MAX;
+            if (sign == -1 && (num > 0 || num < INT_MIN)) return INT_MIN;
         }
-        if (pos)
-        {
-            if (ans < -max)
-            {
-                ans = max;
-            }
-            else ans = -ans;
-        }
-        if (illegal) ans = 0;
-        return ans;
+        return num;
     }
 };
