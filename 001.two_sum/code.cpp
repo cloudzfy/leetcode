2c5f132b3408e6e05caae4075f4482b1fa68f732
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-21 11:18:59 +0800

    Create code.cpp

--- 001.two_sum/code.cpp
+++ 001.two_sum/code.cpp
@@ -5,14 +5,14 @@ public:
         vector<int> ans;
         for (int i = 0; i < nums.size(); i++)
         {
-            m[nums[i]]=i;
+            m[nums[i]] = i;
         }
         for(int i = 0; i < nums.size(); i++)
         {
             if(m.find(target - nums[i]) != m.end() && m[target - nums[i]] != i)
             {
-                ans.push_back(i + 1);
-                ans.push_back(m[target - nums[i]] + 1);
+                ans.push_back(i);
+                ans.push_back(m[target - nums[i]]);
                 break;
             }
         }
