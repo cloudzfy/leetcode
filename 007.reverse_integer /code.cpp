cd78028fb0f9b1f09c8a6961573af03c099b0c3b
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-21 16:08:04 +0800

    Create code.cpp

--- 007.reverse_integer /code.cpp
+++ 007.reverse_integer /code.cpp
@@ -1,49 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-        bool minus = false;
-        if (x < 0)
+        long long ans = 0;
+        while (x)
         {
-            minus = true;
-            x = -x;
-            if (x == -2147483648)
-            {
-                return 0;
-            }
-        }
-        int ans = 0;
-        int max = 214748364;
-        while (x != 0)
-        {
-            if (minus)
-            {
-                if (ans > max || (ans == max && x > 8))
-                {
-                    ans = 0;
-                    break;
-                }
-                else if (ans == max && x == 8)
-                {
-                    ans = - ans - x;
-                    minus = false;
-                    break;
-                }
-            }
-            else
-            {
-                if (ans > max || (ans == max && x > 7))
-                {
-                    ans = 0;
-                    break;
-                }
-            }
             ans = ans * 10 + x % 10;
-            x = x / 10;
+            x /= 10;
         }
-        if (minus)
-        {
-            ans = - ans;
-        }
+        if (ans > INT_MAX || ans < INT_MIN) return 0;
         return ans;
     }
 };
