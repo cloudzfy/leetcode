889b6000084c327705749a63d6f28fae9bdb859e
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-22 16:54:14 +0800

    Create code.cpp

---
+++ 280.wiggle_sort/code.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for (int i = 0; i + 1 < nums.size(); i++) {
+            if (i % 2 == 0 && nums[i] > nums[i + 1]) swap(nums[i], nums[i + 1]);
+            if (i % 2 == 1 && nums[i] < nums[i + 1]) swap(nums[i], nums[i + 1]);
+        }
+    }
+};
