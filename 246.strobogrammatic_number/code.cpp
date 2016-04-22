0c83d5778f7a9e2f2296fbe8b2e20092542032b3
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-22 16:14:48 +0800

    Create code.cpp

---
+++ 246.strobogrammatic_number/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isStrobogrammatic(string num) {
+        int left = 0, right = num.length() - 1;
+        while (left <= right) {
+            if (num[left] == '1' && num[right] == '1') {left++; right--;}
+            else if (num[left] == '8' && num[right] == '8') {left++; right--;}
+            else if (num[left] == '0' && num[right] == '0') {left++; right--;}
+            else if (left < right && num[left] == '6' && num[right] == '9') {left++; right--;}
+            else if (left < right && num[left] == '9' && num[right] == '6') {left++; right--;}
+            else return false;
+        }
+        return true;
+    }
+};
