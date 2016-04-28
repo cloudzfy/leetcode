d9d5a103b573f018c5ac7283f8dada90c8b4af00
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-28 17:05:38 +0800

    Create code2.cpp

---
+++ 291.word_pattern_ii/code2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    vector<string> p2s;
+    unordered_map<string, char> s2p;
+public:
+    bool wordPatternMatch(string pattern, string str) {
+        p2s.resize(26, "");
+        return backtracking(pattern, 0, str, 0);
+    }
+    
+    bool backtracking(string pattern, int idxp, string str, int idxs) {
+        if (idxp == pattern.length()) return idxs == str.length();
+        if (p2s[pattern[idxp] - 'a'] != "") {
+            string match = p2s[pattern[idxp] - 'a'];
+            if (str.substr(idxs, match.length()) == match) {
+                return backtracking(pattern, idxp + 1, str, idxs + match.length());
+            }
+            return false;
+        }
+        for (int i = idxs; i < str.length(); i++) {
+            if (pattern.length() - idxp > str.length() - i) break;
+            string tmp = str.substr(idxs, i - idxs + 1);
+            if (s2p.find(tmp) == s2p.end()) {
+                s2p[tmp] = pattern[idxp];
+                p2s[pattern[idxp] - 'a'] = tmp;
+                if (backtracking(pattern, idxp + 1, str, i + 1)) return true;
+                s2p.erase(tmp);
+                p2s[pattern[idxp] - 'a'] = "";
+            }
+        }
+        return false;
+    }
+};
