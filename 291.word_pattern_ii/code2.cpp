36d9d2f3d2bf997e084ba332139dca8f48dad52a
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-28 17:07:29 +0800

    Create code2.cpp

--- 291.word_pattern_ii/code2.cpp
+++ 291.word_pattern_ii/code2.cpp
@@ -1,16 +1,15 @@
 class Solution {
-    vector<string> p2s;
+    unordered_map<char, string> p2s;
     unordered_map<string, char> s2p;
 public:
     bool wordPatternMatch(string pattern, string str) {
-        p2s.resize(26, "");
         return backtracking(pattern, 0, str, 0);
     }
     
     bool backtracking(string pattern, int idxp, string str, int idxs) {
         if (idxp == pattern.length()) return idxs == str.length();
-        if (p2s[pattern[idxp] - 'a'] != "") {
-            string match = p2s[pattern[idxp] - 'a'];
+        if (p2s.find(pattern[idxp]) != p2s.end()) {
+            string match = p2s[pattern[idxp]];
             if (str.substr(idxs, match.length()) == match) {
                 return backtracking(pattern, idxp + 1, str, idxs + match.length());
             }
@@ -21,10 +20,10 @@ public:
             string tmp = str.substr(idxs, i - idxs + 1);
             if (s2p.find(tmp) == s2p.end()) {
                 s2p[tmp] = pattern[idxp];
-                p2s[pattern[idxp] - 'a'] = tmp;
+                p2s[pattern[idxp]] = tmp;
                 if (backtracking(pattern, idxp + 1, str, i + 1)) return true;
                 s2p.erase(tmp);
-                p2s[pattern[idxp] - 'a'] = "";
+                p2s.erase(pattern[idxp]);
             }
         }
         return false;
