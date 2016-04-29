13dd96a2ef6281618c9810d733d70b14651a4683
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-04-29 11:56:06 +0800

    Create code.cpp

--- 004.median_of_two_sorted_arrays/code.cpp
+++ 004.median_of_two_sorted_arrays/code.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = (int)nums1.size();
-        int n = (int)nums2.size();
-        if ((m + n) % 2 == 1)
-        {
-            return find(nums1, m, nums2, n, (m + n) / 2 + 1);
-        }
-        else
-        {
-            return (find(nums1, m, nums2, n, (m + n) / 2)
-                    + find(nums1, m, nums2, n, (m + n) / 2 + 1)) / 2;
-        }
+        int n = nums1.size();
+        int m = nums2.size();
+        if ((n + m) % 2) return findKthSortedArrays(nums1, n, nums2, m, (n + m + 1) / 2);
+        else return (findKthSortedArrays(nums1, n, nums2, m, (n + m) / 2) 
+            + findKthSortedArrays(nums1, n, nums2, m, (n + m) / 2 + 1)) / 2;
     }
-    double find(vector<int>& nums1, int m, vector<int>& nums2, int n, int k)
-    {
-        if (m > n)
-        {
-            return find(nums2, n, nums1, m, k);
-        }
-        if (m == 0)
-        {
-            return nums2[k - 1 + nums2.size() - n];
-        }
-        if (k <= 1)
-        {
-            return min(nums1[nums1.size() - m], nums2[nums2.size() - n]);
-        }
-        int pa = min(k / 2, m);
-        int pb = k - pa;
-        if (nums1[pa - 1 + nums1.size() - m] < nums2[pb - 1 + nums2.size() - n])
-        {
-            return find(nums1, m - pa, nums2, n, k - pa);
-        }
-        else if (nums1[pa - 1 + nums1.size() - m] > nums2[pb - 1 + nums2.size() - n])
-        {
-            return find(nums1, m, nums2, n - pb, k - pb);
-        }
-        else
-        {
-            return nums1[pa - 1];
+    
+    double findKthSortedArrays(vector<int>& nums1, int n, vector<int>& nums2, int m, int k) {
+        if (n > m) return findKthSortedArrays(nums2, m, nums1, n, k);
+        if (n == 0) return nums2[nums2.size() - m + k - 1];
+        if (k == 1) return min(nums1[nums1.size() - n], nums2[nums2.size() - m]);
+        int i = min(k / 2, n);
+        int j = k - i;
+        if (nums1[nums1.size() - n + i - 1] < nums2[nums2.size() - m + j - 1]) {
+            return findKthSortedArrays(nums1, n - i, nums2, m, k - i);
+        } else {
+            return findKthSortedArrays(nums1, n, nums2, m - j, k - j);
         }
     }
 };
