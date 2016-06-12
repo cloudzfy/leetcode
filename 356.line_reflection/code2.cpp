dc58868c99f2a82e44d24a8b1f8ec78e72437ade
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-06-12 17:19:15 +0800

    Create code2.cpp

--- 356.line_reflection/code2.cpp
+++ 356.line_reflection/code2.cpp
@@ -3,18 +3,16 @@ public:
     bool isReflected(vector<pair<int, int>>& points) {
         int N = points.size();
         if (N == 0) return true;
-        unordered_map<int, unordered_set<int> > m;
+        set<pair<int, int> > m;
         int smallest = INT_MAX, largest = INT_MIN;
         for (int i = 0; i < N; i++) {
-            m[points[i].first].insert(points[i].second);
+            m.insert(points[i]);
             smallest = min(smallest, points[i].first);
             largest = max(largest, points[i].first);
         }
         int sum = smallest + largest;
         for (int i = 0; i < N; i++) {
-            int oppo = sum - points[i].first;
-            if (m.find(oppo) != m.end() && m[oppo].find(points[i].second) != m[oppo].end()) continue;
-            return false;
+            if (m.find(make_pair(sum - points[i].first, points[i].second)) == m.end()) return false;
         }
         return true;
     }
