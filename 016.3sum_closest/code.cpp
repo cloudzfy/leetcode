f26448516596612529fc5dcd27207f2c7762a520
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-06-13 17:43:47 +0800

    Create code.cpp

--- 016.3sum_closest/code.cpp
+++ 016.3sum_closest/code.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int ans = nums[0] + nums[1] + nums[2];
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            int j = i + 1, k = nums.size() - 1;
-            while (j < k) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) j++;
-                else if (k < nums.size() - 1 && nums[k] == nums[k + 1]) k--;
-                else {
-                    int tmp = nums[i] + nums[j] + nums[k];
-                    if (abs(tmp - target) < abs(ans - target)) {
-                        ans = tmp;
-                    }
-                    if (tmp > target) k--;
-                    else if (tmp < target) j++;
-                    else break;
-                }
+        int n = nums.size(), ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                while (left < right && left > i + 1 && nums[left] == nums[left - 1]) left++;
+                while (left < right && right < n - 1 && nums[right] == nums[right + 1]) right--;
+                if (left >= right) break;
+                int sum = nums[i] + nums[left] + nums[right];
+                if (abs(ans - target) > abs(sum - target)) ans = sum;
+                if (sum < target) left++;
+                else if (sum > target) right--;
+                else break;
             }
-            if(ans == target) break;
+            if (ans == target) break;
         }
         return ans;
     }
