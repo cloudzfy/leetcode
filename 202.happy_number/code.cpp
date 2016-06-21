d844a03a3e622c637253a10112f8cebda370e356
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-06-21 11:55:11 +0800

    Create code.cpp

--- 202.happy_number/code.cpp
+++ 202.happy_number/code.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if (n <= 0) return false;
-        if (n == 1) return true;
         unordered_set<int> s;
-        int target = n;
-        do {
-            int tmp = target;
-            target = 0;
-            while (tmp != 0) {
-                target += (tmp % 10) * (tmp % 10);
-                tmp /= 10;
+        while (s.find(n) == s.end()) {
+            s.insert(n);
+            int tmpn = n;
+            n = 0;
+            while (tmpn) {
+                int tmp = tmpn % 10;
+                tmpn /= 10;
+                n += tmp * tmp;
             }
-            if (s.find(target) != s.end()) return false;
-            s.insert(target);
-        } while(target != 1);
-        return true;
+            if (n == 1) return true;
+        }
+        return false;
     }
 };
