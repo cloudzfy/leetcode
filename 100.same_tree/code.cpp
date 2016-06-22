b4c8db4f2d567801f85ec86a1669afafed84f727
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-06-22 17:58:51 +0800

    Create code.cpp

--- 100.same_tree/code.cpp
+++ 100.same_tree/code.cpp
@@ -11,10 +11,7 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (p == NULL && q == NULL) return true;
-        if (p != NULL && q == NULL) return false;
-        if (p == NULL && q != NULL) return false;
-        if (p->val == q->val) {
-            return isSameTree(p->left, q->left) & isSameTree(p->right, q->right);
-        } else return false;
+        if (p == NULL || q == NULL) return false;
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
