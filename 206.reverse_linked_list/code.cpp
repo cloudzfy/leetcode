d9b1bce52e52950a5aee97a8d58fbb51c764db84
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-06-28 15:57:23 +0800

    Create code.cpp

--- 206.reverse_linked_list/code.cpp
+++ 206.reverse_linked_list/code.cpp
@@ -9,16 +9,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head;
-        ListNode *p1 = head, *p2 = p1->next, *p3 = p2->next;
-        while (true) {
-            p2->next = p1;
-            if (p3 == NULL) break;
-            p1 = p2;
-            p2 = p3;
-            p3 = p3->next;
+        ListNode *pre = NULL;
+        while (head) {
+            ListNode *next = head->next;
+            head->next = pre;
+            pre = head;
+            head = next;
         }
-        head->next = NULL;
-        return p2;
+        return pre;
     }
 };
