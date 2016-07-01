ce3b6f2774f91faf1df3f06581cfd0d7cfa74925
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-07-01 10:48:56 +0800

    Create code.cpp

---
+++ 371.sum_of_two_integers/code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b) {
+            int c = a & b;
+            a ^= b;
+            b = c << 1;
+        }
+        return a;
+    }
+};
